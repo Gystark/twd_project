f1ca584d0e7787f6aa990e7787a7c127e2eeca94
Gabor Daniel Borics-Kurti <2209013b@student.gla.ac.uk>
2017-01-27 14:53:31 +0000

    Finished chapter 8.

--- rango/urls.py
+++ rango/urls.py
@@ -6,5 +6,7 @@ from rango import views
 
 urlpatterns = [url('^$', views.index, name="index"),
                url(r'^about/$', views.about, name="about"),
-               url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.view_category, name="view_category")
+               url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name="add_page"),
+               url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.view_category, name="view_category"),
+               url(r'^add_category/$', views.add_category, name="add_category"),
                ]
--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from rango.models import Category, Page
+from .forms import CategoryForm, PageForm
 
 
 def index(request):
@@ -32,3 +33,47 @@ def view_category(request, category_name_slug):
         context["pages"] = None
 
     return render(request, "rango/category.html", context)
+
+
+def add_category(request):
+    """
+    Create a new category.
+    """
+    form = CategoryForm()
+
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+
+            return index(request)
+        else:
+            print form.errors
+    return render(request, "rango/add_category.html", {"form": form})
+
+
+def add_page(request, category_name_slug):
+    """
+    Create a new page.
+    """
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    form = PageForm()
+
+    if request.method == "POST":
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return view_category(request, category_name_slug)
+        else:
+            print form.errors
+    return render(request, "rango/add_page.html", {"form": form, "category": category})
